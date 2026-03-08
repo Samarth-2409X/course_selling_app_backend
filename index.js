86c2a7181dd6cd2d3a4b5f06c753d594a53631c7
Samarth-2409X <samarthgone667@gmail.com>
2026-03-08 19:37:50 +0530

    updating routing

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {createUserRoute} = requier("./user.js");
+
+const app = express();
+
+createUserRoute(app);
+
+app.post("course/purchse", (req, res) => {
+    res.json({
+        msg:"nothing"
+    })
+});
+
+app.get("course/preview", (req, res) => {
+    res.json({
+        msg:"nothing"
+    })
+});
