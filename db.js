1111317fa260e2e0433a7f948ef9d756d7cb18fc
Samarth-2409X <samarthgone667@gmail.com>
2026-03-08 23:50:57 +0530

    adding DB scheam

---
+++ db.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = ({
+    email:{type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const adminSchema = ({
+    email:{type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const courseSchema = ({
+    title: String,
+    discription: String,
+    price: Number,
+    imgurl: String,
+    creatorId: ObjectId
+})
+
+const purchaseSchema = ({
+    userId: ObjectId,
+    courseId: ObjectId
+})
+
+const userModel = mongoose.Model("user", userSchema);
+const adminModel = mongoose.Model("admin", adminSchema);
+const courseModel = mongoose.Model("course", courseSchema);
+const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
