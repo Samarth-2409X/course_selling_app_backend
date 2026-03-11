22b8c4680e259211485da33978e3f38c6259b55a
Samarth-2409X <samarthgone667@gmail.com>
2026-03-11 22:49:41 +0530

    created course router and middleware

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,50 @@
 const {Router} = require("express");
 const adminRouter = Router();
-const {adminModel} = require("../db");
+const {adminModel, userModel} = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
  
-adminRouter.post("/signup", (res, req) => {
+adminRouter.post("/signup", async(res, req) => {
+    const {email, password, firstName, lastName} = req.body;
+
+    await adminModel.create({
+        email:email,
+        password:password,
+        firstName:firstName,
+        lastName:lastName
+    })
+
     res.json({
-        msg:"nothing"
+        msg:"user is successfully signed up"
     })
 })
 
-adminRouter.post("/signin", (res, req) => {
-    res.json({
-        msg:"nothing"
+adminRouter.post("/signin", async(res, req) => {
+    const {email, password} = req.body;
+
+    const admin = await userModel.findOne({
+        email,
+        password
     })
+
+    if(admin){
+        const token = jwt.sign({
+            id:user._id,
+        }, JWT_ADMIN_PASSWORD);
+
+        res.json({
+            token:token
+        })
+    }
+    else{
+        res.status(403).json({
+            mgs:"Incorrect credentials"
+        })
+    }
 })
 
 adminRouter.post("/course", (res, req) => {
+
     res.json({
         msg:"nothing"
     })
--- routes/course.js
+++ routes/course.js
@@ -1,16 +1,29 @@
 const {Router} = require("express");
 const courseRouter = Router();
+const {purchaseModel, courseModel} = require("../db");
+const {userMiddleware} = require("../middlewares");
 
-	courseRouter.post("/purchse", (req, res) => {
+	courseRouter.post("/purchse", userMiddleware, async(req, res) => {
+        const userId = req.userId;
+        const courseId = req.body.courseId;
+
+        await courseModel.create({
+            userId,
+            courseId
+        })
+
         res.json({
-            msg:"nothing"
+            msg:"you have successfully bought the course"
         })
     });
 
-    courseRouter.get("/preview", (req, res) => {
+    courseRouter.get("/preview", async(req, res) => {
+        const courses = await purchaseModel.find({})
+
         res.json({
-            msg:"nothing"
+            courses
         })
+        
     });
 
 
