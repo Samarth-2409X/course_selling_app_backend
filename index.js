7c3a90a0ae60c02ff66a38c1617a97467cf5e7b3
Samarth-2409X <samarthgone667@gmail.com>
2026-03-11 23:29:16 +0530

    added gitignore file

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+require("dotenv").config;
+
 const express = require("express");
+const {mongoose} = require("mongoose");
+const app = express();
+
 const { courseRouter } = require("./routes/course.js");
 const { userRouter } = require("./routes/user.js");
 const { adminRouter } = require("./routes/admin.js");
-const {mongoose} = require("mongoose");
-
-
-const app = express();
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 async function main(){
-    await mongoose.connect("mongodb+srv://samarth:Samarth7777@cluster0.z0adlk5.mongodb.net/courseApp");
+    await mongoose.connect(process.env.MONGO_URL);
     app.listen(3000);
-    console.log("listening on port 3000");
+    
 }
 
 main();
